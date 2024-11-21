37e12d05b52ed09812bcdd926d88e0c65921d458
ramalakshmi-icanio <ramalakshmi.r@icanio.com>
2024-11-21 15:05:22 +0530

    seperate a form

--- src/pages/community/onboarding/EditCommunity.jsx
+++ src/pages/community/onboarding/EditCommunity.jsx
@@ -1,12 +1,12 @@
 import {
   Button,
   FormLabel,
-  MenuItem,
   Select,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material";
-import { Grid, Stack } from "@mui/system";
+import { Grid } from "@mui/system";
 import AppCard from "components/AppComponents/AppCard";
 
 import { useFormik } from "formik";
@@ -49,138 +49,6 @@ const EditCommunity = ({ open, onClose }) => {
     setErrors,
   } = formik;
 
-  const sections = [
-    {
-      title: "Address Details",
-      fields: [
-        {
-          label: "Community Name",
-          type: "text",
-          value: values.addressDetails.communityName,
-          name: "addressDetails.communityName",
-        },
-        {
-          label: "City",
-          type: "text",
-          value: values.addressDetails.city,
-          name: "addressDetails.city",
-        },
-        {
-          label: "State",
-          type: "text",
-          value: values.addressDetails.state,
-          name: "addressDetails.state",
-        },
-        {
-          label: "ZipCode",
-          type: "text",
-          value: values.addressDetails.zipcode,
-          name: "addressDetails.zipcode",
-        },
-      ],
-    },
-    {
-      title: "Community Details",
-      fields: [
-        {
-          label: "Community Property Manager",
-          type: "select",
-          value: values.communityDetails.communityManager,
-          name: "communityDetails.communityManager",
-          options: [
-            { label: "Ten", value: 10 },
-            { label: "Twenty", value: 20 },
-            { label: "Thirty", value: 30 },
-          ],
-        },
-        {
-          label: "Contact Number",
-          type: "text",
-          value: values.communityDetails.contactNumber,
-          name: "communityDetails.contactNumber",
-        },
-        {
-          label: "Community Type",
-          type: "select",
-          value: values.communityDetails.communityType,
-          name: "communityDetails.communityType",
-          options: [
-            { label: "Ten", value: 10 },
-            { label: "Twenty", value: 20 },
-            { label: "Thirty", value: 30 },
-          ],
-        },
-        {
-          label: "Email ID",
-          type: "text",
-          value: values.communityDetails.email,
-          name: "communityDetails.email",
-        },
-      ],
-    },
-    {
-      title: "Community Details",
-      fields: [
-        {
-          label: "Community Property Manager",
-          type: "select",
-          value: values.communityDetails.communityManager,
-          name: "communityDetails.communityManager",
-          options: [
-            { label: "Ten", value: 10 },
-            { label: "Twenty", value: 20 },
-            { label: "Thirty", value: 30 },
-          ],
-        },
-        {
-          label: "Contact Number",
-          type: "text",
-          value: values.communityDetails.contactNumber,
-          name: "communityDetails.contactNumber",
-        },
-        {
-          label: "Community Type",
-          type: "select",
-          value: values.communityDetails.communityType,
-          name: "communityDetails.communityType",
-          options: [
-            { label: "Ten", value: 10 },
-            { label: "Twenty", value: 20 },
-            { label: "Thirty", value: 30 },
-          ],
-        },
-        {
-          label: "Email ID",
-          type: "text",
-          value: values.communityDetails.email,
-          name: "communityDetails.email",
-        },
-      ],
-    },
-    {
-      title: "Insurance Details",
-      fields: [
-        {
-          label: "Insurance Value",
-          type: "select",
-          value: values.insuranceDetails.insuranceValue,
-          name: "insuranceDetails.insuranceValue",
-          options: [
-            { label: "Ten", value: 10 },
-            { label: "Twenty", value: 20 },
-            { label: "Thirty", value: 30 },
-          ],
-        },
-        {
-          label: "Insurance Coverage",
-          type: "text",
-          value: values.insuranceDetails.insuranceCoverage,
-          name: "insuranceDetails.insuranceCoverage",
-        },
-      ],
-    },
-  ];
-
   const Footer = () => {
     return (
       <>
@@ -192,53 +60,175 @@ const EditCommunity = ({ open, onClose }) => {
 
   return (
     <AppCard title={"Desert Springs"} footer={<Footer />}>
-      {sections.map((section, index) => (
-        <Grid
-          container
-          size={{ xs: 12 }}
-          key={index}
-          padding={1}
-          spacing={2}
-          direction={"column"}
-        >
-          <Typography variant="h3" sx={{ mb: 2 }}>
-            {section.title}
-          </Typography>
-          <Grid container spacing={2}>
-            {section.fields.map((field, idx) => (
-              <Grid item size={{ xs: 6 }}>
-                <Stack rowGap={1} key={idx}>
-                  <FormLabel>{field.label}</FormLabel>
-                  {field.type === "text" && (
-                    <TextField
-                      value={field.value}
-                      fullWidth
-                      onChange={handleChange}
-                      name={field.name}
-                    />
-                  )}
-                  {field.type === "select" && (
-                    <Select
-                      value={field.value}
-                      fullWidth
-                      onChange={(event) => {
-                        handleChange(event);
-                      }}
-                      name={field.name}
-                    >
-                      {field.options.map((option, optionIdx) => (
-                        <MenuItem key={optionIdx} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  )}
-                </Stack>
-              </Grid>
-            ))}
+      <Grid
+        container
+        size={{ xs: 12 }}
+        padding={1}
+        spacing={2}
+        direction={"column"}
+      >
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          {"AddressDetails"}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Community Name</FormLabel>
+
+              <TextField
+                value={values.addressDetails.communityName}
+                fullWidth
+                onChange={handleChange}
+                name="addressDetails.communityName"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>City</FormLabel>
+
+              <TextField
+                value={values.addressDetails.city}
+                fullWidth
+                onChange={handleChange}
+                name="addressDetails.city"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>State</FormLabel>
+
+              <TextField
+                value={values.addressDetails.state}
+                fullWidth
+                onChange={handleChange}
+                name="addressDetails.state"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>ZipCode</FormLabel>
+
+              <TextField
+                value={values.addressDetails.zipcode}
+                fullWidth
+                onChange={handleChange}
+                name="addressDetails.zipcode"
+              />
+            </Stack>
           </Grid>
         </Grid>
-      ))}
+      </Grid>
+      <Grid
+        container
+        size={{ xs: 12 }}
+        padding={1}
+        spacing={2}
+        direction={"column"}
+      >
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          {"CommunityDetails"}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Community Property Manager</FormLabel>
+
+              <Select
+                value={values.communityDetails.communityManager}
+                fullWidth
+                onChange={handleChange}
+                options={[
+                  { label: "Ten", value: 10 },
+                  { label: "Twenty", value: 20 },
+                  { label: "Thirty", value: 30 },
+                ]}
+                name="communityDetails.communityManager"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Contact Number</FormLabel>
+
+              <TextField
+                value={values.communityDetails.contactNumber}
+                fullWidth
+                onChange={handleChange}
+                name="communityDetails.contactNumber"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Community Type</FormLabel>
+
+              <Select
+                value={values.communityDetails.communityType}
+                fullWidth
+                onChange={handleChange}
+                name="communityDetails.communityType"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Email ID</FormLabel>
+
+              <TextField
+                value={values.communityDetails.email}
+                fullWidth
+                onChange={handleChange}
+                name="communityDetails.email"
+              />
+            </Stack>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        size={{ xs: 12 }}
+        padding={1}
+        spacing={2}
+        direction={"column"}
+      >
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          {"InsuranceDetails"}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Insurance Value</FormLabel>
+
+              <Select
+                value={values.insuranceDetails.insuranceValue}
+                fullWidth
+                onChange={handleChange}
+                options={[
+                  { label: "Ten", value: 10 },
+                  { label: "Twenty", value: 20 },
+                  { label: "Thirty", value: 30 },
+                ]}
+                name="insuranceDetails.insuranceValue"
+              />
+            </Stack>
+          </Grid>
+          <Grid item size={{ xs: 6 }}>
+            <Stack rowGap={1}>
+              <FormLabel>Insurance Coverage</FormLabel>
+
+              <TextField
+                value={values.insuranceDetails.insuranceCoverage}
+                fullWidth
+                onChange={handleChange}
+                name="insuranceDetails.insuranceCoverage"
+              />
+            </Stack>
+          </Grid>
+        </Grid>
+      </Grid>
     </AppCard>
   );
 };
