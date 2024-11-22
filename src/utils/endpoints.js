ac625ba2f37ae403ee54e111aa5d1f6e18ad5054
ramalakshmi-icanio <ramalakshmi.r@icanio.com>
2024-11-22 15:58:39 +0530

    changes

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,13 +1,14 @@
 const LOGIN = {
   getUser: "todos",
-  userLogin:"api/auth/login",
-  requestReset:(email)=>`/api/auth/request-reset?email=${email}`,
-  resetPassword:"/api/auth/resetPassword",
-  newPassword:"/api/auth/setNewPassword",
-  enableMailChecking:(email)=>`/api/auth/email?email=${email}`,
+  userLogin: "api/auth/login",
+  requestReset: (email) => `/api/auth/request-reset?email=${email}`,
+  resetPassword: "/api/auth/resetPassword",
+  newPassword: "/api/auth/setNewPassword",
+  enableMailChecking: (email) => `/api/auth/email?email=${email}`,
 };
-const ONBOARD={
-  getUsers:"/api/veruna/users"
-}
+const ONBOARD = {
+  getUsers: "/api/veruna/users",
+  getUserById: (id) => `/api/community/:${id}`,
+};
 
-export { LOGIN,ONBOARD };
+export { LOGIN, ONBOARD };
