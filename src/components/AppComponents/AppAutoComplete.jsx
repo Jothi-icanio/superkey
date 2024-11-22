b7672bfb4ff5236b62f8b9f268a1c0008520561a
ramalakshmi-icanio <ramalakshmi.r@icanio.com>
2024-11-22 16:10:15 +0530

    changes

--- src/components/AppComponents/AppAutoComplete.jsx
+++ src/components/AppComponents/AppAutoComplete.jsx
@@ -1,179 +1,177 @@
 import styled from "@emotion/styled";
 import {
-    Autocomplete,
-    Box,
-    Divider,
-    TextField,
-    Typography,
-    createFilterOptions,
-    useTheme,
+  Autocomplete,
+  Box,
+  Divider,
+  TextField,
+  Typography,
+  createFilterOptions,
+  useTheme,
 } from "@mui/material";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const AddManuallyButton = styled(Typography)({
-    color: "#2954E1",
-    cursor: "pointer",
-    marginLeft: "auto",
-    fontSize: "0.875rem",
-    fontWeight: 500,
+  color: "#2954E1",
+  cursor: "pointer",
+  marginLeft: "auto",
+  fontSize: "0.875rem",
+  fontWeight: 500,
 });
 
 const filter = createFilterOptions();
 
 const AppAutoComplete = ({
-    freeSolo = true,
-    options = [],
-    nameParam = "label",
-    placeholder = 'type',
-    inputValue,
-    error,
-    onChange,
-    name,
-    value,
-    onSearch,
-    searchString,
-    ...props
+  freeSolo = true,
+  options = [],
+  nameParam = "label",
+  placeholder = "type",
+  inputValue,
+  error,
+  onChange,
+  name,
+  value,
+  onSearch,
+  searchString,
+  ...props
 }) => {
-    const [open, setOpen] = useState(false)
-    const theme = useTheme()
-    const handleAddManually = (customOption) => {
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
+  const handleAddManually = (customOption) => {
+    onChange(name, customOption);
+    onSearch("");
 
-        onChange(name, customOption);
-        onSearch('');
-
-        setOpen(false);
-
+    setOpen(false);
+  };
+  const handleClose = () => {
+    setOpen(false);
+    onSearch("");
+  };
+  const handleInputChange = (_, newInputValue, reason) => {
+    if (reason == "input") {
+      onSearch(newInputValue);
     }
-    const handleClose = () => {
-        setOpen(false);
-        onSearch('');
-
-    };
-    const handleInputChange = (_, newInputValue, reason) => {
-        if (reason == 'input') {
-            onSearch(newInputValue);
+  };
+  return (
+    <Autocomplete
+      {...props}
+      name={name}
+      value={value}
+      options={options}
+      clearOnBlur
+      openOnFocus
+      freeSolo={freeSolo}
+      onChange={(_, value) => onChange(name, value)}
+      onInputChange={handleInputChange}
+      getOptionLabel={(option) => {
+        if (typeof option === "string") {
+          return option;
         }
-    }
-    return (
-        <Autocomplete
-            {...props}
+        return option[nameParam] || "";
+      }}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params);
+        if (freeSolo && params.inputValue !== "") {
+          return [
+            {
+              id: "add-manually",
+              [nameParam]: params.inputValue,
+              isCustom: true,
+            },
+            ...filtered,
+          ];
+        }
 
-            name={name}
-            value={value}
-            options={options}
-            clearOnBlur
-            openOnFocus
-            freeSolo={freeSolo}
-            onChange={(_, value) => onChange(name, value)}
-            onInputChange={handleInputChange}
-            getOptionLabel={(option) => {
-                if (typeof option === "string") {
-                    return option;
-                }
-                return option[nameParam] || "";
-            }}
-            filterOptions={(options, params) => {
-                const filtered = filter(options, params);
-                if (freeSolo && (params.inputValue !== "")) {
-                    return [
-                        { id: "add-manually", [nameParam]: params.inputValue, isCustom: true },
-                        ...filtered,
-                    ];
-                }
-
-                return filtered;
-            }}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    placeholder={placeholder}
-                    error={Boolean(error)}
-                    helperText={error}
-                />
-            )}
-            renderOption={(props, option) => {
-                if (option?.isCustom) {
-                    return (
-                        <>
-                            <Box
-                                {...props}
-                                key={option.id}
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "space-between",
-                                    cursor: "default !important", // Prevent click event on entire option
-                                    "&:hover": {
-                                        backgroundColor: "transparent !important", // Prevent hover color change
-                                    },
-                                }}
-                                onClick={(event) => event.stopPropagation()}
-                            >
-                                <Typography variant="subtitle1">{option[nameParam]}</Typography>
-                                <AddManuallyButton
-                                    role="button"
-                                    tabIndex={0}
-                                    onClick={(event) => {
-
-                                        handleAddManually(option)
-                                    }}
-                                >
-                                    Add Manually
-                                </AddManuallyButton>
-                            </Box>
-                            <Divider />
-                        </>
-                    );
-                }
-
-                return (
-                    <>
-                        <Box
-                            {...props}
-                            key={option.id} // Add a key for each option
-                            sx={{ px: 2 }}
-                        >
-                            <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-                                {option[nameParam]}
-                            </Typography>
-                        </Box>
-                        <Divider />
-                    </>
-
-                );
-            }}
-            ListboxProps={{
-                sx: {
-                    padding: 0, // Removes the default padding
-                    "& .MuiAutocomplete-option": {
-                        minHeight: "45px", // Set a consistent height for each option
-                        display: "flex",
-                        alignItems: "center", // Ensures text is vertically aligned
-                        "&:hover, &:focus": {
-                            color: "inherit",
-                            backgroundColor: theme.palette.info.light, // Change background color on hover for regular options
-                        },
-
-                    },
-                },
-            }}
-            componentsProps={{
-                paper: {
-                    sx: {
-                        marginTop: 2, // Add gap
-                        borderRadius: 2,
-                        maxHeight: "153px", // Approximately 3 items of 51px height each
-                        overflowY: "auto", // Enable scrolling if more than 3 items
-                    },
-                },
-            }}
-            fullWidth
-            open={open}
-            onOpen={() => setOpen(true)}
-            onClose={() => {
-                handleClose()
-            }}
+        return filtered;
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder={placeholder}
+          error={Boolean(error)}
+          helperText={error}
         />
-    );
+      )}
+      renderOption={(props, option) => {
+        if (option?.isCustom) {
+          return (
+            <>
+              <Box
+                {...props}
+                key={option.id}
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  overflow: "hidden",
+                  cursor: "default !important", // Prevent click event on entire option
+                  "&:hover": {
+                    backgroundColor: "transparent !important", // Prevent hover color change
+                  },
+                }}
+                onClick={(event) => event.stopPropagation()}
+              >
+                <Typography variant="subtitle1">{option[nameParam]}</Typography>
+                <AddManuallyButton
+                  role="button"
+                  tabIndex={0}
+                  onClick={(event) => {
+                    handleAddManually(option);
+                  }}
+                >
+                  Add Manually
+                </AddManuallyButton>
+              </Box>
+              <Divider />
+            </>
+          );
+        }
+
+        return (
+          <>
+            <Box
+              {...props}
+              key={option.id} // Add a key for each option
+              sx={{ px: 2 }}
+            >
+              <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                {option[nameParam]}
+              </Typography>
+            </Box>
+            <Divider />
+          </>
+        );
+      }}
+      ListboxProps={{
+        sx: {
+          maxHeight: "153px", // Approximately 3 items of 51px height each
+
+          padding: 0, // Removes the default padding
+          "& .MuiAutocomplete-option": {
+            minHeight: "45px", // Set a consistent height for each option
+            display: "flex",
+            alignItems: "center", // Ensures text is vertically aligned
+            "&:hover, &:focus": {
+              color: "inherit",
+              backgroundColor: theme.palette.info.light, // Change background color on hover for regular options
+            },
+          },
+        },
+      }}
+      componentsProps={{
+        paper: {
+          sx: {
+            marginTop: 2, // Add gap
+            borderRadius: 2,
+          },
+        },
+      }}
+      fullWidth
+      open={true}
+      onOpen={() => setOpen(true)}
+      onClose={() => {
+        handleClose();
+      }}
+    />
+  );
 };
 
 export default AppAutoComplete;
--- src/pages/community/onboarding/CommunityAddress.jsx
+++ src/pages/community/onboarding/CommunityAddress.jsx
@@ -1,39 +1,62 @@
-import { Grid2 as Grid, Stack } from '@mui/material';
-import mapImg from 'assets/images/icons/map.png';
-import AppAutoComplete from 'components/AppComponents/AppAutoComplete';
-import AppDropDown from 'components/AppComponents/AppDropDown';
-import AppLabelComponent from 'components/AppComponents/AppLabelComponent';
-import { Image, StyledTypography } from 'components/StyledComponents';
-import { useState } from 'react';
+import { Grid2 as Grid } from "@mui/material";
+import mapImg from "assets/images/icons/map.png";
+import AppAutoComplete from "components/AppComponents/AppAutoComplete";
+import AppLabelComponent from "components/AppComponents/AppLabelComponent";
+import { Image } from "components/StyledComponents";
+import { useState } from "react";
 
 const options = [
-    { label: "Phoenix North Estates, Phoenix, AZ 85023, USAPhoenix North Estates, Phoenix, AZ 85023, USA", value: "AZ" },
-    { label: "New York, Phoenix, NY 54321, USA", value: " " },
-    { label: "India, Phoenix", value: "LA" },
-    { label: "New ", value: " lj" }
+  {
+    label:
+      "Phoenix North Estates, Phoenix, AZ 85023, USAPhoenix North Estates, Phoenix, AZ 85023, USA",
+    value: "AZ",
+  },
+  { label: "New York, Phoenix, NY 54321, USA", value: " " },
+  { label: "India, Phoenix", value: "LA" },
+  { label: "New ", value: " lj" },
+  { label: "New1", value: " d" },
+  { label: "New ", value: " 5" },
+  { label: "New ", value: " 7" },
+  { label: "New ", value: " 8" },
 ];
 
-const CommunityAddress = ({ setFieldValue, formValues, touched, errors, }) => {
+const CommunityAddress = ({ setFieldValue, formValues, touched, errors }) => {
+  const [address, setAddress] = useState("");
+  const onSearch = (searchString) => {
+    console.log(searchString);
+    setAddress(searchString);
+  };
+  return (
+    <Grid
+      container
+      textAlign={"center"}
+      justifyContent={"center"}
+      rowSpacing={4}
+    >
+      <Grid item size={{ xs: 10 }}>
+        <Image height={"20vh"} src={mapImg} alt="map" />
+      </Grid>
+      <Grid item>
+        <AppLabelComponent
+          gap={2}
+          variant="h4"
+          label={"What is the address of your community?"}
+        >
+          <AppAutoComplete
+            name="communityAddress"
+            freesolo
+            error={touched.communityAddress && errors.communityAddress}
+            onChange={setFieldValue}
+            searchString={address}
+            value={formValues.communityAddress}
+            options={options}
+            placeholder="Search your address"
+            onSearch={onSearch}
+          />
+        </AppLabelComponent>
+      </Grid>
+    </Grid>
+  );
+};
 
-    const [address, setAddress] = useState('')
-    const onSearch = (searchString) => {
-        console.log(searchString)
-        setAddress(searchString)
-    }
-    return (
-        <Grid container textAlign={'center'} justifyContent={'center'} rowSpacing={4} >
-            <Grid item size={{ xs: 10 }}>
-                <Image height={'20vh'} src={mapImg} alt='map' />
-            </Grid>
-            <Grid item >
-                <AppLabelComponent gap={2} variant="h4" label={'What is the address of your community?'}>
-                    <AppAutoComplete name='communityAddress' freesolo error={touched.communityAddress && errors.communityAddress} onChange={setFieldValue} searchString={address} value={formValues.communityAddress} options={options} placeholder='Search your address' onSearch={onSearch} />
-                </AppLabelComponent>
-
-
-            </Grid>
-        </Grid>
-    )
-}
-
-export default CommunityAddress
+export default CommunityAddress;
